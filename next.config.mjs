13b2ef35f14307d586bafbfaea1fda3dfe84fa60
sebastien Amafou <sebastien.amafou@gmail.com>
2026-03-19 22:03:30 +0100

    feat: initial commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,26 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+// next.config.mjs
 
-export default nextConfig;
+import withPWAInit from 'next-pwa';
+
+const withPWA = withPWAInit({
+  dest: 'public', // Le Service Worker sera compilé dans le dossier public
+  disable: process.env.NODE_ENV === 'development', // Ne pas utiliser le cache PWA en dev pour faciliter le débug
+  register: true, // Le client enregistre automatiquement le SW
+  skipWaiting: true, // Mise à jour automatique de la PWA sans recharger manuellement
+});
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    // Si tu utilises Supabase Storage pour les images de tes burgers plus tard
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**.supabase.co', // Autorise le domaine Supabase
+      },
+    ],
+  },
+};
+
+export default withPWA(nextConfig);
